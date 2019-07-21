1683f438adb003d134cc0fce9cce784bc2b46d2d
thinh <thinhlx1993@gmail.com>
2019-07-21 23:20:20 +0700

    save

--- example.py
+++ example.py
@@ -22,30 +22,35 @@
 import gym
 import holdem
 
+
 def play_out_hand(env, n_seats):
-  # reset environment, gather relevant observations
-  (player_states, (community_infos, community_cards)) = env.reset()
-  (player_infos, player_hands) = zip(*player_states)
+    # reset environment, gather relevant observations
+    (player_states, (community_infos, community_cards)) = env.reset()
+    (player_infos, player_hands) = zip(*player_states)
 
-  # display the table, cards and all
-  env.render(mode='human')
-
-  terminal = False
-  while not terminal:
-    # play safe actions, check when noone else has raised, call when raised.
-    actions = holdem.safe_actions(community_infos, n_seats=n_seats)
-    (player_states, (community_infos, community_cards)), rews, terminal, info = env.step(actions)
+    # display the table, cards and all
     env.render(mode='human')
 
-env = gym.make('TexasHoldem-v1') # holdem.TexasHoldemEnv(2)
+    terminal = False
+    while not terminal:
+        # play safe actions, check when noone else has raised, call when raised.
+        actions = holdem.safe_actions(community_infos, n_seats=n_seats)
+        (player_states, (community_infos, community_cards)), rews, terminal, info = env.step(actions)
+        print("rews: {}".format(rews))
+        env.render(mode='human')
+
+
+env = gym.make('TexasHoldem-v1')  # holdem.TexasHoldemEnv(2)
 
 # start with 2 players
-env.add_player(0, stack=2000) # add a player to seat 0 with 2000 "chips"
-env.add_player(1, stack=2000) # add another player to seat 1 with 2000 "chips"
+env.add_player(0, stack=2000)  # add a player to seat 0 with 2000 "chips"
+env.add_player(1, stack=2000)  # add another player to seat 1 with 2000 "chips"
 # play out a hand
 play_out_hand(env, env.n_seats)
 
 # add one more player
-env.add_player(2, stack=2000) # add another player to seat 1 with 2000 "chips"
+env.add_player(2, stack=2000)  # add another player to seat 1 with 2000 "chips"
+# add one more player
+env.add_player(3, stack=2000)  # add another player to seat 1 with 2000 "chips"
 # play out another hand
 play_out_hand(env, env.n_seats)
